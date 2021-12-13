1339c09c1b834be8cc14b34a596fb42e6b489778
Samhuw8a <Samuel.huwiler@gmx.ch>
2021-12-13 13:56:56 +0100

    Update

--- handler.py
+++ handler.py
@@ -1,36 +1,22 @@
-
 import json
-import render as r
-
-
 
 class handler:
-
-    
-    def __init__(self,path):
-
+    def __init__(self,path,render):
         #öffnen von json
         with open(path,"r") as f:
             self.data=json.load(f)   
-
-        
-        self.render=r.render()
-
+        self.render=render.render()
         #erstellen von Variabeln
         self.frame_data=""
         self.next_frame_data=""
         self.text_color=""
         self.text_data=""
 
-
-     
     def print_frame(self):
-            #Konsole leeren
-            self.render.clear()
-
-            #text ausgeben
-            self.render.print(self.text_data,self.text_color)
-
+        #Konsole leeren
+        self.render.clear()
+        #text ausgeben
+        self.render.print(self.text_data,self.text_color)
 
     def set_variables(self,index):
         #zuweisung von Variabeln
@@ -38,57 +24,27 @@ class handler:
             self.next_frame_data=self.frame_data["next"]
             self.text_data=self.frame_data["text"]
             self.text_color=self.frame_data["color"]
-            
-
-
     
     def load_by_keyword(self,action):
-        
         try:
             #laden von neuem frameindex
-            
-            index=self.next_frame_data[action]
-            #überprüfen ob END
-            if index=="END":
+            if self.next_frame_data[action] =="END":
                 return index
-            
             #Variabeln neu setzen
             self.set_variables(index)
-
             #Text ausgeben
             self.print_frame()
-            
             #True zurückgeben falls fehlerlos
             return True
 
-            
-
-
         except:
-
             #printen von fehlertext
             self.render.print("Unbekannte Option",color="red")
-
             #False zurückgeben falls fehler aufgetreten sind
             return False
 
-
-        
-    
-
-    
     def load_by_index(self,index):
-
         #setzen der Variabeln
         self.set_variables(index)
-
         #Text ausgeben
         self.print_frame()
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-
 import handler as h
-
-
-
+import render as r
 #main Funktion
 def main():
-
     #erstellen von Framehandler
-    frame_handler=h.handler("data.json")
-
+    frame_handler=h.handler("data.json",r)
     #laden des ersten Frames
     frame_handler.load_by_index("1")
-
-    
     while True:
-
-       
-        ret=frame_handler.load_by_keyword((frame_handler.render.input()).lower())
-
-        
-        if ret=="END":
+        if frame_handler.load_by_keyword((frame_handler.render.input()).lower()) == "End":
             break
-
-    
-
-
-
-
 if __name__=='__main__':
- 
-    #aufrufen von main Funktion
     main()
